5db2cc590bf65dd2327294f7dbf279ce27e3b886
Asozykina <a.sozykina@gmail.com>
2020-10-04 21:11:13 +0300

    Update storage.py
    back to V0

--- storage.py
+++ storage.py
@@ -12,15 +12,11 @@ class Storage:
         else:
             return None
 
-    def remove(self, key):
-        if key in self.data:
-             del self.data[key]
+    def remove(self):
+        pass
 
-    def set(self,key,value):
-        if key in self.data:
-            self.data[key] = value
-        else:
-            print('key_not_found')
-        
+    def set(self):
+        pass
+    
     def add(self):
         pass
