a1194367b69e891c89063e91df8f80430f536a69
Asozykina <a.sozykina@gmail.com>
2020-10-04 21:11:49 +0300

    Update tests.py
    back to V0

--- tests.py
+++ tests.py
@@ -4,17 +4,10 @@ def test_add():
     pass
 
 def test_remove():
-    st = Storage({'a': 1, 'b': 2})
-    key = 'a'
-    val = st.remove(key)
-    assert val is None, "Value for the key {} is not removed"
+    pass
 
 def test_set():
-    st = Storage({'a': 1, 'b': 2})
-    key = 'b'
-    val = 3
-    st.set(key,val)
-    assert st.data[key] == 3, "Value for the key {} is not equal to set {} ".format(key, val)
+    pass
 
 def test_get():
     st = Storage({'a': 1, 'b': 2})
